d7b070d9d07ad718197afbf9d8269b2730d300f9
Ulisandro <sanchezulises952@gmail.com>
2023-12-27 16:44:22 -0300

    cambios

--- database/db.js
+++ database/db.js
@@ -5,9 +5,13 @@ dotenv.config();
 
 
 
- const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST,DB_PORT} = process.env;
+//  const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST,DB_PORT} = process.env;
 
-
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_PASSWORD = process.env.DB_PASSWORD || 'ciclon1031'
+const DB_PORT = process.env.DB_PORT || 3306
+const DB_NAME = process.env.DB_NAME || 'root'
+const DB_USER = process.env.DB_USER || 'root'
 
 
 const db = new Sequelize({
